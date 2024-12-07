458225632b57d8ec2119e179794c62fca5d5c278
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-12-07 21:58:41 +0600

    console.log("Answers to the questions 1"); /*1) Write a function named calculateDifference that takes two arguments and returns the difference between the first and second argument.*/ function calculateDifference(a, b) {     return a - b; } const result = calculateDifference(10, 5); console.log(result);

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Answers to the questions 1");
+/*1) Write a function named calculateDifference that takes two arguments and returns the difference between the first and second argument.*/
+function calculateDifference(a, b) {
+    return a - b;
+}
+const result = calculateDifference(10, 5);
+console.log(result); 
